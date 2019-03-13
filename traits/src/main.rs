6fe9a0b4a8e4cde540b8db6e24290ad8e63e1149
Cheng <liucheng.tech@outlook.com>
2019-03-13 14:05:35 +0800

    :star2: traits

---
+++ traits/src/main.rs
@@ -0,0 +1,135 @@
+use std::fmt::Display;
+use core::fmt::Debug;
+
+fn main() {
+    trait_demo();
+}
+
+// Traits
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    // non-default implementaion
+    // fn summarize(&self) -> String;
+    // default implementation
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+    // using default implementation
+    // fn summarize(&self) -> String {
+    //     format!("{}, by {} ({})", self.headline, self.author, self.location)
+    // }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn trait_demo() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
+    };
+
+    println!("New article available! {}", article.summarize());
+}
+
+// traits as arguments
+fn notify(item: impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// trait bounds
+fn notify2<T: Summary>(item1: T, item2: T) {
+
+}
+
+// multiple traits
+fn notify3(item: impl Summary + Display) {
+
+}
+
+// mutliple traits
+fn notify4<T: Summary + Display>(item: T) {
+
+}
+
+// where clause, alternative syntax for specifying trait bounds
+fn some_function<T, U>(t: T, u: U) -> i32
+where T: Display + Clone,
+      U: Clone + Debug {
+    1
+}
+
+// return traits
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// conditional implement methods
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self {
+            x,
+            y,
+        }
+    }
+}
+
+// impl only when trait bounds to Display + PartialOrd
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
