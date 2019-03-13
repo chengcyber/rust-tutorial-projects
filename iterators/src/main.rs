b06c89ab450c03d17cd2e3f70f4db3855f5fd199
Cheng <liucheng.tech@outlook.com>
2019-03-13 22:12:29 +0800

    :star2: iterators

---
+++ iterators/src/main.rs
@@ -0,0 +1,112 @@
+fn main() {
+    for_in();
+    collect();
+    closures();
+}
+
+fn for_in() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+}
+
+fn collect() {
+    let v1 = vec![1, 2, 3];
+
+    // NOTE iter is lazy calculation, there is no effects to run the next line
+    let v2 = v1.iter().map(|x| x + 1);
+
+    // using collect to create a collection
+    let v2: Vec<_> = v2.collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
+}
+
+fn closures() {
+
+    #[derive(PartialEq, Debug)]
+    struct Shoe {
+        size: u32,
+        style: String,
+    }
+
+    fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+        // into_iter takes the ownership and returns owned values
+        shoes.into_iter()
+            // colsure capture shoe_size in its environment
+            .filter(|s| s.size == shoe_size)
+            .collect()
+    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 13, style: String::from("sandal") },
+            Shoe { size: 10, style: String::from("boot") },
+        ];
+
+        let in_my_size = shoes_in_my_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe { size: 10, style: String::from("sneaker") },
+                Shoe { size: 10, style: String::from("boot") },
+            ]
+        )
+    }
+}
+
+fn iterator_trait() {
+    struct Counter {
+        count: u32,
+    }
+
+    impl Counter {
+        fn new() -> Counter {
+            Counter { count: 0 }
+        }
+    }
+
+    impl Iterator for Counter {
+        type Item = u32;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.count += 1;
+
+            if self.count < 6 {
+                Some(self.count)
+            } else {
+                None
+            }
+        }
+    }
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
+
+        assert_eq!(18, sum);
+    }
+
+}
